a787835bdb3db78b0d82de6fb3ea70aa56b42923
tuna204 <85708853+tuna204@users.noreply.github.com>
2025-11-19 17:50:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,6 @@ def download(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
+
+
